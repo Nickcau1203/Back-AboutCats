92142108c6d97af9fac898bbadddeb72d34939f3
Nicole Cau <nicolecau.tds2.senai@gmail.com>
2025-09-04 11:04:14 -0300

    feat: index.routes.js

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,10 +2,8 @@ import express from "express";
 
 // Importar todas as rotas
 import authRouter from "./auth.routes.js";
-import animesRouter from "./animeRoutes.js";
-import personagensRouter from "./personagemRoutes.js";
-import collectionRouter from "./collectionRoutes.js";
 import cardRouter from "./cardRoutes.js";
+import gatoRouter from "./gatoRoutes.js"; // Importar as rotas de gatos
 
 import authMiddleware from "../middleware/authMiddleware.js";
 
@@ -13,13 +11,11 @@ const router = express.Router();
 
 // Rotas públicas
 router.use("/auth", authRouter);
-router.use("/collections", collectionRouter);
 router.use("/cards", cardRouter);
 
 // Rotas protegidas
 router.use(authMiddleware);
 
-router.use("/animes", animesRouter);
-router.use("/personagens", personagensRouter);
+router.use("/gatos", gatoRouter); // Adicionar as rotas de gatos
 
 export default router;
